d51a0927c92d9a9be7faa94f4c064098302ae3a5
Colin McGraw <colinmcgraw@me.com>
2018-10-05 10:38:00 -0700

    contacts class

--- index.js
+++ index.js
@@ -2,6 +2,11 @@
 
 const Auth = require('./lib/class.auth.js');
 const Blog = require('./lib/class.blog.js');
+const BlogAuthors = require('./lib/class.blogauthors.js');
+const BlogComments = require('./lib/class.blogcomments.js');
+const BlogPosts = require('./lib/class.blogposts.js');
+const BlogTopics = require('./lib/class.blogtopics.js');
+const Contacts = require('./lib/class.contacts.js');
 const Files = require('./lib/class.files.js');
 const Forms = require('./lib/class.forms.js');
 const HubDb = require('./lib/class.hubdb.js');
@@ -11,6 +16,11 @@ const Templates = require('./lib/class.templates.js');
 module.exports = { 
   Auth,
   Blog,
+  BlogAuthors,
+  BlogComments,
+  BlogPosts,
+  BlogTopics,
+  Contacts,
   Files,
   Forms,
   HubDb,
